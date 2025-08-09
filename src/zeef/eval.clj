f293b33308227fe9740329e56ca34eb072577310
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2025-08-09 14:01:09 +0200

    refactor eval: add translate-expression, use str alias for clojure.string

--- src/zeef/eval.clj
+++ src/zeef/eval.clj
@@ -1,6 +1,6 @@
 (ns zeef.eval
   (:require [clojure.spec.alpha :as s]
-            [clojure.string]
+            [clojure.string :as str]
             [clojure.walk :as walk]
             [zeef.core :as zf]
             [zeef.specs]
@@ -19,15 +19,15 @@
 
 (defmethod transform-condition :starts-with?
   [[_ x substr]]
-  `(clojure.string/starts-with? ~x ~substr))
+  `(str/starts-with? ~x ~substr))
 
 (defmethod transform-condition :ends-with?
   [[_ x substr]]
-  `(clojure.string/ends-with? ~x ~substr))
+  `(str/ends-with? ~x ~substr))
 
 (defmethod transform-condition :includes?
   [[_ x substr]]
-  `(clojure.string/includes? ~x ~substr))
+  `(str/includes? ~x ~substr))
 
 (defmethod transform-condition :between?
   [[_ x lo hi]]
@@ -66,8 +66,8 @@
     (transform-condition expr)
     (transform-operation expr)))
 
-(defn compile-expression
-  "Compiles symbolic expression `expr` into a predicate function.
+(defn translate-expression
+  "Translates symbolic expression `expr` into a symbolic form
     
    `expr` has to conform to zeef's expression syntax.
    
@@ -84,12 +84,52 @@
    - `:expr` - The invalid expression
    - `:explanation` - A string explaining why the expression doesn't match the
      spec
+   - `:spec` - The spec that was used for validation"
+  ([expr]
+   (translate-expression expr get))
+  ([expr resolver]
+   (when-not (zf/expression? expr)
+     (throw (ex-info "Invalid expression."
+                     {:expr expr
+                      :explanation (s/explain-str ::zs/expression expr)
+                      :spec ::zs/expression})))
+   (let [m (gensym)]
+     `(fn [~m]
+        ~(walk/postwalk
+          (fn [node]
+            (if (instance? Field node)
+              `(~resolver ~m ~(.name node))
+              node))
+          (transform-expression expr))))))
+
+(defn compile-expression
+  "Compiles symbolic expression `expr` into a predicate function.
+    
+   `expr` has to conform to zeef's expression syntax.
+   
+   If the expression contains fields, these are resolved by calling `resolver`
+   during evaluation. `resolver` is called with the argument passed to the
+   predicate function and the name of the field as a keyword.
+   
+   Internally, `translate-expression` is used to generate the symbolic form of
+   the expression, which is then evaluated using `eval` to produce the predicate
+   function.
+
+   There are two arities:
+   - `(compile-expression expr)` uses `clojure.core/get` as the default resolver.
+   - `(compile-expression expr resolver)` allows specifying a custom resolver.
+
+   Throws an `ExceptionInfo` with message \"Invalid expression.\" if `expr` is
+   not a valid expression. The exception contains additional information:
+   - `:expr` - The invalid expression
+   - `:explanation` - A string explaining why the expression doesn't match the
+     spec
    - `:spec` - The spec that was used for validation
 
    Example:
 
    ```
-   (let [pred (compile-expression [:between? (zeef.core/field :id) 1 3] get)
+   (let [pred (compile-expression [:between? (zeef.core/field :id) 1 3])
          data [{:id 1 :text \"a\"}
                {:id 3 :text \"b\"}
                {:id 5 :text \"c\"}]]
@@ -98,17 +138,5 @@
   ([expr]
    (compile-expression expr get))
   ([expr resolver]
-   (when-not (zf/expression? expr)
-     (throw (ex-info "Invalid expression."
-                     {:expr expr
-                      :explanation (s/explain-str ::zs/expression expr)
-                      :spec ::zs/expression})))
-   (let [m (gensym)
-         form `(fn [~m]
-                 ~(walk/postwalk
-                   (fn [node]
-                     (if (instance? Field node)
-                       `(~resolver ~m ~(.name node))
-                       node))
-                   (transform-expression expr)))]
+   (let [form (translate-expression expr resolver)]
      (eval form))))
